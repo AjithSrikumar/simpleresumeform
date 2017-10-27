30f0694f47f311bc987c88804042a34c236d3d48
srikumar ravindran <sriku27@gmail.com>
2017-10-27 16:11:20 +0530

    Resume form ready for deployment.

---
+++ validation.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    /* 
+    var x = document.forms["resume"]["firstname"].value;
+    var y = document.forms["resume"]["lastname"].value;
+    if (x == "") {
+        alert("First Name must be filled out");
+        return false;
+    }
+   else if (y==""){
+        alert("Last Name must be filled out");
+        return false;
+    }
+  */
+}
